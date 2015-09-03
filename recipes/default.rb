021bc132694809fc8b215ec91db33f3a52e34374
Paul Morton <pmorton@biaprotect.com>
2015-09-02 17:57:17 -0700

    Wrong var

--- recipes/default.rb
+++ recipes/default.rb
@@ -26,7 +26,7 @@ end
 
 kibana_web kib.name do
   type 'nginx'
-  listen_port kip.nginx_listen_port
+  listen_port kib.nginx_listen_port
   server_name node.name
   template 'kibana-nginx_file.conf.erb'
 end
